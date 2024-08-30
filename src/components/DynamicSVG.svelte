4ca4f0b10650355a31ec0cae601a76228239adfc
Loudbook <loudbooked@gmail.com>
2024-08-29 22:46:50 -0400

    Rewrite

---
+++ src/components/DynamicSVG.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let iconName = "";
+  
+    let svgIcon: ConstructorOfATypedSvelteComponent | null = null;
+
+    loadIcon();
+
+    async function loadIcon() {
+        if (iconName) {
+            try {
+                svgIcon = (await import(`../lib/icons/${iconName}.svelte`)).default;
+            } catch (e) {
+                console.error("Component not found:", e);
+                svgIcon = null;
+            }
+        }
+    }
+  </script>
+  
+  {#if svgIcon}
+    <svelte:component this={svgIcon} />
+  {/if}
