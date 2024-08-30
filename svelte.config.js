bf8077204e9a12e0b7d291297313a0542046df41
Loudbook <loudbooked@gmail.com>
2024-08-29 23:01:03 -0400

    GitHub Pages Support

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,11 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
